40da8999b211b2b50332c851f0396d35058d2410
ashotaro <shotaro.akima01@g.softbank.co.jp>
2019-07-10 14:53:49 +0900

    [clean] (filter_footprint) Refactor the declaration of class

--- src/range_filter_footprint.cpp
+++ src/range_filter_footprint.cpp
@@ -9,10 +9,11 @@ int main(int argc, char **argv) {
       ros::this_node::getName() + "/footprint_topic", "/footprint");
   std::vector<std::string> range_topics = nh.param<std::vector<std::string>>(
       ros::this_node::getName() + "/range_topics", {"/range_topics"});
-  std::vector<RangeFilterFootprint *> RFF(range_topics.size());
+  std::vector<range_filter_footprint::RangeFilterFootprint *> RFF(range_topics.size());
+  range_filter_footprint::SubFootprint SF(nh, footprint_topic);
 
   for (int i = 0; i < range_topics.size(); i++)
-    RFF[i] = new RangeFilterFootprint(nh, range_topics[i], footprint_topic);
+    RFF[i] = new range_filter_footprint::RangeFilterFootprint(nh, range_topics[i], SF);
 
   ros::spin();
 };
